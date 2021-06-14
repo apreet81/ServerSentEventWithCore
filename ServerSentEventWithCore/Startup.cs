4739ddcc77db530a8254d0fec68a470c36c658de
Amanpreet <apreet81@live.com>
2021-06-14 12:34:10 +0530

    Added another approach related URL

--- ServerSentEventWithCore/Startup.cs
+++ ServerSentEventWithCore/Startup.cs
@@ -12,6 +12,9 @@ namespace ServerSentEventWithCore
 {
     public class Startup
     {
+        // Need to check : https://dzone.com/articles/server-sent-events-sse-support-for-aspnet-core
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
